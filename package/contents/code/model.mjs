0db9ec0b145178a232efd8effefd315e3d29c5bb
Krzysztof <3818002+korapp@users.noreply.github.com>
2026-04-30 04:44:32 +0200

    refactor(model): extract subset of updated properties

--- package/contents/code/model.mjs
+++ package/contents/code/model.mjs
@@ -3,31 +3,35 @@ import { formatIfNumber } from "./formatter.mjs"
 const activeStates = ['on', 'open', 'idle']
 const noValueStates = ['unknown', 'unavailable']
 
-function getDisplayValue({ state, attribute, attributes, unit, value_number_precision }) {
+function getDisplayValue({ attributes, state }, config = {}) {
+    const { attribute, value_number_precision } = config
     if (attribute && attributes[attribute]) return attributes[attribute] + ''
     if (!state) return ''
+    const unit = attributes.unit_of_measurement
     if (unit && !noValueStates.includes(state)) return formatIfNumber(state, value_number_precision) + (unit === '%' ? unit : ' ' + unit)
     return state
 }
 
-export function Entity({ entity_id = '', name, icon, state, attribute = '', attributes, unit, display = 1, value_number_precision, default_action = {}, scroll_action = {} } = {}, data = {}) {
-    this.entity_id = entity_id
-    this.attributes = Object.assign({}, attributes, data.a)
-    this.state = data.s || state || ''
-    this.name = name || this.attributes.friendly_name || ''
-    this.icon = icon || this.attributes.icon || ''
-    this.unit = unit || this.attributes.unit_of_measurement || ''
-    this.attribute = attribute
-    this.default_action = default_action
-    this.scroll_action = scroll_action
-    this.display = display
-    this.value_number_precision = value_number_precision
+export function EntityUpdate(config = {}, update = {}, entity = {}) {
+    this.attributes = Object.assign({}, entity.attributes, update.a)
+    this.icon = config.icon || this.attributes.icon || ''
+    this.state = update.s || entity.state || ''
     this.active = activeStates.includes(this.state)
-    this.domain = entity_id.substring(0, entity_id.indexOf('.'))
-    this.value = getDisplayValue(this)
+    this.value = getDisplayValue(this, config)
 }
 
-export function ConfigEntity({ entity_id = '', name, icon, attribute, display = 1, value_number_precision, default_action, scroll_action, notify } = {}) {
+export function Entity(config = {}, data = {}) {
+    Object.assign(this, new EntityUpdate(config, data))
+    this.entity_id = config.entity_id
+    this.name = config.name || this.attributes.friendly_name || ''
+    this.attribute = config.attribute || ''
+    this.default_action = config.default_action || {}
+    this.scroll_action = config.scroll_action || {}
+    this.display = config.display ?? 1
+    this.domain = config.domain || ''
+}
+
+export function ConfigEntity(config = {}) {
     Object.defineProperties(this, {
         entity_id: {
             enumerable: true,
@@ -48,15 +52,15 @@ export function ConfigEntity({ entity_id = '', name, icon, attribute, display = 
     addActionProperty(this, 'default_action')
     addActionProperty(this, 'scroll_action')
 
-    this.entity_id = entity_id
-    this.name = name
-    this.icon = icon
-    this.attribute = attribute
-    this.display = display
-    this.default_action = default_action
-    this.scroll_action = scroll_action
-    this.notify = notify
-    this.value_number_precision = value_number_precision
+    this.entity_id = config.entity_id || ''
+    this.name = config.name
+    this.icon = config.icon
+    this.attribute = config.attribute
+    this.display = config.display ?? 1
+    this.default_action = config.default_action
+    this.scroll_action = config.scroll_action
+    this.notify = config.notify
+    this.value_number_precision = config.value_number_precision
 }
 
 function addActionProperty(o, name) {
