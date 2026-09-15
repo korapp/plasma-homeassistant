6dd4a1085de57ea00986633a504fb213c63cae8a
Krzysztof <3818002+korapp@users.noreply.github.com>
2026-09-15 15:03:16 +0200

    fix: use custom range in scroll action for climate

--- package/contents/code/serviceOverrides.mjs
+++ package/contents/code/serviceOverrides.mjs
@@ -1,4 +1,13 @@
-const SERVICE_FIELDS_TO_ATTRIBUTES = Object.freeze({})
+const SERVICE_FIELDS_TO_ATTRIBUTES = Object.freeze({
+  climate: {
+    set_temperature: {
+      temperature: {
+        max: 'max_temp',
+        min: 'min_temp'
+      }
+    }
+  }
+})
 
 export function getFieldsForService(domain, service) {
   return SERVICE_FIELDS_TO_ATTRIBUTES[domain]?.[service] ?? {}
